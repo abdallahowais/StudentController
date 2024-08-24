a61f884d076c2672cd5345f4fa5d4a30ebb1d593
abdallahowais <abdallah.owis@students.alquds.edu>
2024-08-24 19:43:43 +0300

    first commit

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentController.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult GetAll()
+        {
+            return View();
+        }
+        public IActionResult Update()
+        {
+            return View();
+        }
+
+    }
+}
